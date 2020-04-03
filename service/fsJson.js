debcff8bd31f786c5679371064c6bc746c797c44
JWang <1528406176@qq.com>
2020-04-03 12:00:56 +0800

    goods商品数据提纯

---
+++ service/fsJson.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+// 初步提纯
+fs.readFile("./data_json/goods.json", "utf8", function(err, data) {
+  let newData = JSON.parse(data); // 格式化内容
+  let i = 0;
+  let pushData = [];
+  // RECORDS json中的对象名称
+  newData.RECORDS.map((value, index) => {
+    //   image有图片地址的就加进去
+    if (value.IMAGE1 !== null) {
+      i++;
+      pushData.push(value);
+    }
+  });
+  fs.writeFile("./newJson.json", JSON.stringify(pushData), function(err) {
+    if (err) console.log("失败");
+    else console.log("写入成功");
+  });
+});
